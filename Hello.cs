6be19c122fc5021039148e68c2391b3d7fe69d56
tristan.jardon <tristan.jardon@gmail.com>
2017-11-01 12:58:43 +0100

    test02

---
+++ Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            // Com inutile
+            // Com inutile 2
+        }
+    }
+}
